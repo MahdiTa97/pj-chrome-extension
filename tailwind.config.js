7b2a06cacb6d916804481d4e0199f4de136bdc02
mahdita97 <taala.mahdy@gmail.com>
2022-04-30 12:10:19 +0430

    feat: Add daisyui lib

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['var(--font-sans)', ...defaultTheme.fontFamily.sans],
+    },
   },
-  plugins: [],
+  plugins: [require('daisyui')],
   variants: {},
   corePlugins: {
     preflight: true,
+  },
+  daisyui: {
+    rtl: true,
   },
 };
