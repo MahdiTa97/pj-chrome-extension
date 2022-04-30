8bd3043d5d6e775ebc26b7192b215d764a35e28e
mahdita97 <taala.mahdy@gmail.com>
2022-04-30 12:11:39 +0430

    feat: Improve popup

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from '../../assets/img/logo.svg';
 import './Popup.css';
 
 const Popup = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
+    <div
+      class="card w-96 bg-slate-800 text-primary-content rounded-none"
+      dir="rtl"
+    >
+      <div class="card-body">
+        <h2 class="card-title">پژوهیار</h2>
+        <p>برای استفاده از افزونه ابتدا باید وارد بشوید!</p>
+        <div class="card-actions justify-end">
+          <div className="card-body">
+            <button className="btn btn-primary">ورود</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
