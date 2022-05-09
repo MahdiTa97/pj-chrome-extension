7186b14c90a90092ce47ec9b92047c4d47486aaa
mahdita97 <taala.mahdy@gmail.com>
2022-05-09 18:27:19 +0430

    feat: Add logout alert component

---
+++ src/components/popupComponents/LogoutAlert/LogoutAlert.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Text } from '../../ui';
+import { InformationCircleIcon } from '@heroicons/react/outline';
+
+interface Props {
+  message: string;
+  setAlert: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const LogoutAlert = (props: Props) => {
+  const { message, setAlert } = props;
+  return (
+    <div className="alert">
+      <div>
+        <InformationCircleIcon className="stroke-info w-6 h-6" />
+        <div>
+          <Text variant="h3" className="font-bold">
+            new_message
+          </Text>
+          <Text className="text-xs">{message}</Text>
+        </div>
+      </div>
+      <div className="flex-none">
+        <button className="btn btn-sm" onClick={() => setAlert(false)}>
+          <Text>see</Text>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default LogoutAlert;
--- src/components/popupComponents/index.ts
+++ src/components/popupComponents/index.ts
@@ -1,2 +1,3 @@
 export { default as Login } from './Login';
 export { default as Panel } from './Panel';
+export { default as LogoutAlert } from './LogoutAlert';
