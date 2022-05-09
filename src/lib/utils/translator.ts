74f78eef984f16fdc475c07d92794d103ed801be
mahdita97 <taala.mahdy@gmail.com>
2022-05-09 18:26:41 +0430

    feat: Add english lang to translator function

--- src/lib/utils/translator.ts
+++ src/lib/utils/translator.ts
@@ -1,11 +1,14 @@
 import fa from '../../assets/locales/fa/common.json';
+import en from '../../assets/locales/en/common.json';
 
 const resources = {
   fa: fa,
+  en: en,
 };
 
 const t = (text: string) => {
   const selectedResource: any = resources['fa'];
+
   return selectedResource[text] ? selectedResource[text] : text;
 };
 
