9eb24aaaaa47627258faeceee094b6eb15d90c66
mahdita97 <taala.mahdy@gmail.com>
2022-05-31 12:31:50 +0430

    refactor: Remove unused functions

--- src/pages/Content/index.ts
+++ src/pages/Content/index.ts
@@ -1,9 +1,6 @@
 import { messagesHandler } from './modules/messagesHandler';
-import { printLine } from './modules/print';
 
 console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
-
-printLine("Using the 'printLine' function from the Print Module");
 
 messagesHandler();
