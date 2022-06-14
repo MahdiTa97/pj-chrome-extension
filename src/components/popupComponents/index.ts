91e9cb98a5218e580ffd19d94a9d625c8651cf9d
mahdita97 <taala.mahdy@gmail.com>
2022-06-14 11:16:22 +0430

    fix: not support component to popup index

--- src/components/popupComponents/index.ts
+++ src/components/popupComponents/index.ts
@@ -3,3 +3,4 @@ export { default as Panel } from './Panel';
 export { default as LogoutAlert } from './LogoutAlert';
 export { default as DropDown } from './DropDown';
 export { default as TranslatorView } from './TranslatorView';
+export { default as NotSupport } from './NotSupport';
